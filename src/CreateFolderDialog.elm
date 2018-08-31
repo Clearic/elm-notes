25014d215b7952818235a5d924271dbf41b9f34c
Alexandr <alexandr@fakemail.com>
2018-08-31 10:16:36 +0500

    add folder name validation

--- src/CreateFolderDialog.elm
+++ src/CreateFolderDialog.elm
@@ -1,16 +1,20 @@
 module CreateFolderDialog exposing (..)
 
 import Html exposing (Html, button, div, form, i, input, text)
-import Html.Attributes exposing (class, disabled, hidden, placeholder, type_, value)
+import Html.Attributes exposing (class, classList, disabled, hidden, placeholder, type_, value)
 import Html.Events exposing (onClick, onInput, onSubmit)
 
 
 type alias Model =
-    { name : String }
+    { name : String
+    , nameValMsg : Maybe String
+    }
 
 
 initModel =
-    { name = "" }
+    { name = ""
+    , nameValMsg = Nothing
+    }
 
 
 type Msg
@@ -29,13 +33,49 @@ update : Msg -> Model -> ( Model, ExternalMsg )
 update msg model =
     case msg of
         ChangeName name ->
-            ( { model | name = name }, NoOp )
+            ( { model | name = name, nameValMsg = validateName name }, NoOp )
 
         Close ->
             ( model, Cancel )
 
         Submit ->
-            ( model, CreateFolder model.name )
+            { model | nameValMsg = validateName model.name }
+                |> (\x ->
+                        case x.nameValMsg of
+                            Nothing ->
+                                ( model, CreateFolder model.name )
+
+                            Just _ ->
+                                ( x, NoOp )
+                   )
+
+
+validateName : String -> Maybe String
+validateName name =
+    if String.length name == 0 then
+        Just "Folder name must not be empty"
+    else
+        Nothing
+
+
+isModelValid : Model -> Bool
+isModelValid model =
+    case model.nameValMsg of
+        Nothing ->
+            True
+
+        _ ->
+            False
+
+
+isNothing : Maybe a -> Bool
+isNothing x =
+    case x of
+        Nothing ->
+            True
+
+        _ ->
+            False
 
 
 view : Model -> Html Msg
@@ -49,10 +89,30 @@ view model =
             , div [ class "popup__content" ]
                 [ form [ onSubmit Submit ]
                     [ div [ class "form-row" ]
-                        [ input [ class "popup__input", placeholder "Folder Name", value model.name, onInput ChangeName ] []
-                        , button [ class "popup__button", disabled (String.length model.name == 0), type_ "submit" ] [ text "Create" ]
+                        [ input
+                            [ classList
+                                [ ( "popup__input", True )
+                                , ( "popup__input--invalid", not <| isNothing model.nameValMsg )
+                                ]
+                            , placeholder "Folder Name"
+                            , value model.name
+                            , onInput ChangeName
+                            ]
+                            []
+                        , button [ class "popup__button", disabled (not <| isModelValid model), type_ "submit" ] [ text "Create" ]
                         ]
+                    , viewValMsg model.nameValMsg
                     ]
                 ]
             ]
         ]
+
+
+viewValMsg : Maybe String -> Html Msg
+viewValMsg mayBeValMsg =
+    case mayBeValMsg of
+        Just valMsg ->
+            div [ class "popup__validation-msg" ] [ text valMsg ]
+
+        Nothing ->
+            text ""
