4535d17c087e819552dd049b1e41b225a422ef56
Alexandr <alexandr@fakemail.com>
2018-09-01 10:14:08 +0500

    re-organize code

--- src/Main.elm
+++ src/Main.elm
@@ -9,6 +9,22 @@ import Html.Events exposing (onClick, onInput, onSubmit)
 import Utils exposing (onClickPreventDefault)
 
 
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { items : Dict String FolderItem
+    , path : List String
+    , openedNote : Maybe String
+    , dialog : Dialog
+    }
+
+
 type alias Note =
     { id : String
     , title : String
@@ -33,22 +49,6 @@ type Dialog
     | DialogCreateFolderDialog CreateFolderDialog.Model
 
 
-main =
-    Browser.sandbox { init = init, update = update, view = view }
-
-
-
--- MODEL
-
-
-type alias Model =
-    { items : Dict String FolderItem
-    , path : List String
-    , openedNote : Maybe String
-    , dialog : Dialog
-    }
-
-
 init : Model
 init =
     { items =
@@ -68,6 +68,49 @@ init =
     }
 
 
+getFolder : Dict String FolderItem -> List String -> Maybe Folder
+getFolder items path =
+    List.head path
+        |> Maybe.andThen (\id -> Dict.get id items)
+        |> Maybe.andThen
+            (\item ->
+                case item of
+                    FolderItemNote _ ->
+                        Nothing
+
+                    FolderItemFolder f ->
+                        Just f
+            )
+
+
+getFolderById : Dict String FolderItem -> String -> Maybe Folder
+getFolderById items id =
+    Dict.get id items
+        |> Maybe.andThen
+            (\item ->
+                case item of
+                    FolderItemNote _ ->
+                        Nothing
+
+                    FolderItemFolder f ->
+                        Just f
+            )
+
+
+getNote : Dict String FolderItem -> String -> Maybe Note
+getNote items id =
+    Dict.get id items
+        |> Maybe.andThen
+            (\item ->
+                case item of
+                    FolderItemNote n ->
+                        Just n
+
+                    FolderItemFolder _ ->
+                        Nothing
+            )
+
+
 
 -- UPDATE
 
@@ -84,40 +127,6 @@ type Msg
 
 type DialogMsg
     = DialogMsgCreateFolderDialog CreateFolderDialog.Msg
-
-
-genNoteTitle : String -> String
-genNoteTitle text =
-    text
-        |> String.lines
-        |> List.head
-        |> Maybe.map String.trim
-        |> Maybe.andThen
-            (\x ->
-                if String.length x == 0 then
-                    Nothing
-                else
-                    Just x
-            )
-        |> Maybe.withDefault "untitled"
-
-
-insertNote : String -> String -> Dict String FolderItem -> Dict String FolderItem
-insertNote id text items =
-    Dict.insert id (FolderItemNote (Note id (genNoteTitle text) text)) items
-
-
-insertFolder : Folder -> Dict String FolderItem -> Dict String FolderItem
-insertFolder folder items =
-    Dict.insert folder.id (FolderItemFolder folder) items
-
-
-getNewId : Dict String FolderItem -> String
-getNewId items =
-    items
-        |> Dict.size
-        |> (\x -> x + 1)
-        |> String.fromInt
 
 
 update : Msg -> Model -> Model
@@ -205,97 +214,51 @@ update msg model =
             { model | dialog = DialogCreateFolderDialog (CreateFolderDialog.initModel folderNames) }
 
 
+genNoteTitle : String -> String
+genNoteTitle text =
+    text
+        |> String.lines
+        |> List.head
+        |> Maybe.map String.trim
+        |> Maybe.andThen
+            (\x ->
+                if String.length x == 0 then
+                    Nothing
+                else
+                    Just x
+            )
+        |> Maybe.withDefault "untitled"
+
+
+insertNote : String -> String -> Dict String FolderItem -> Dict String FolderItem
+insertNote id text items =
+    Dict.insert id (FolderItemNote (Note id (genNoteTitle text) text)) items
+
+
+insertFolder : Folder -> Dict String FolderItem -> Dict String FolderItem
+insertFolder folder items =
+    Dict.insert folder.id (FolderItemFolder folder) items
+
+
+getNewId : Dict String FolderItem -> String
+getNewId items =
+    items
+        |> Dict.size
+        |> (\x -> x + 1)
+        |> String.fromInt
+
+
 
 -- VIEW
 
 
-getFolder : Dict String FolderItem -> List String -> Maybe Folder
-getFolder items path =
-    List.head path
-        |> Maybe.andThen (\id -> Dict.get id items)
-        |> Maybe.andThen
-            (\item ->
-                case item of
-                    FolderItemNote _ ->
-                        Nothing
-
-                    FolderItemFolder f ->
-                        Just f
-            )
-
-
-getFolderById : Dict String FolderItem -> String -> Maybe Folder
-getFolderById items id =
-    Dict.get id items
-        |> Maybe.andThen
-            (\item ->
-                case item of
-                    FolderItemNote _ ->
-                        Nothing
-
-                    FolderItemFolder f ->
-                        Just f
-            )
-
-
-getNote : Dict String FolderItem -> String -> Maybe Note
-getNote items id =
-    Dict.get id items
-        |> Maybe.andThen
-            (\item ->
-                case item of
-                    FolderItemNote n ->
-                        Just n
-
-                    FolderItemFolder _ ->
-                        Nothing
-            )
-
-
-viewFolderItem : FolderItem -> Html Msg
-viewFolderItem folderItem =
-    case folderItem of
-        FolderItemNote note ->
-            li [ class "notes-list__item" ]
-                [ a [ class "notes-list__link", href "#", onClickPreventDefault (OpenNote note) ]
-                    [ text note.title ]
-                ]
-
-        FolderItemFolder folder ->
-            li [ class "notes-list__item notes-list__item--folder" ]
-                [ a [ class "notes-list__link", href "#", onClickPreventDefault (OpenFolder folder) ]
-                    [ text folder.title ]
-                ]
-
-
-viewFolder : Dict String FolderItem -> Folder -> Html Msg
-viewFolder items folder =
-    div [ class "app__left-sidebar" ]
-        [ div [ class "bar" ]
-            [ button [ class "bar__button margin-right", onClick GoBack, hidden (folder.id == "0") ] [ i [ class "icon-angle-left" ] [] ]
-            , div [ class "bar__title" ] [ text folder.title ]
-            , button [ class "bar__button", onClick NewNote ] [ i [ class "icon-file" ] [] ]
-            , button [ class "bar__button", onClick OpenCreateFolderDialog ] [ i [ class "icon-folder" ] [] ]
-            ]
-        , ul [ class "notes-list" ]
-            (folder.items
-                |> List.filterMap (\x -> Dict.get x items)
-                |> List.map viewFolderItem
-            )
+view : Model -> Html Msg
+view model =
+    div [ class "app" ]
+        [ viewNotesList model
+        , viewNoteEdit model
+        , viewDialog model
         ]
-
-
-viewNote : Maybe Note -> Html Msg
-viewNote maybeNote =
-    case maybeNote of
-        Just note ->
-            div [ class "app__main" ]
-                [ div [ class "bar" ] [ div [ class "bar__title" ] [ text note.title ] ]
-                , textarea [ class "note-editor", value note.text, onInput (ChangeNote note.id) ] []
-                ]
-
-        Nothing ->
-            text ""
 
 
 viewNotesList : Model -> Html Msg
@@ -325,10 +288,47 @@ viewDialog model =
             Html.map (\x -> DialogMsg <| DialogMsgCreateFolderDialog x) (CreateFolderDialog.view dialogModel)
 
 
-view : Model -> Html Msg
-view model =
-    div [ class "app" ]
-        [ viewNotesList model
-        , viewNoteEdit model
-        , viewDialog model
+viewFolder : Dict String FolderItem -> Folder -> Html Msg
+viewFolder items folder =
+    div [ class "app__left-sidebar" ]
+        [ div [ class "bar" ]
+            [ button [ class "bar__button margin-right", onClick GoBack, hidden (folder.id == "0") ] [ i [ class "icon-angle-left" ] [] ]
+            , div [ class "bar__title" ] [ text folder.title ]
+            , button [ class "bar__button", onClick NewNote ] [ i [ class "icon-file" ] [] ]
+            , button [ class "bar__button", onClick OpenCreateFolderDialog ] [ i [ class "icon-folder" ] [] ]
+            ]
+        , ul [ class "notes-list" ]
+            (folder.items
+                |> List.filterMap (\x -> Dict.get x items)
+                |> List.map viewFolderItem
+            )
         ]
+
+
+viewFolderItem : FolderItem -> Html Msg
+viewFolderItem folderItem =
+    case folderItem of
+        FolderItemNote note ->
+            li [ class "notes-list__item" ]
+                [ a [ class "notes-list__link", href "#", onClickPreventDefault (OpenNote note) ]
+                    [ text note.title ]
+                ]
+
+        FolderItemFolder folder ->
+            li [ class "notes-list__item notes-list__item--folder" ]
+                [ a [ class "notes-list__link", href "#", onClickPreventDefault (OpenFolder folder) ]
+                    [ text folder.title ]
+                ]
+
+
+viewNote : Maybe Note -> Html Msg
+viewNote maybeNote =
+    case maybeNote of
+        Just note ->
+            div [ class "app__main" ]
+                [ div [ class "bar" ] [ div [ class "bar__title" ] [ text note.title ] ]
+                , textarea [ class "note-editor", value note.text, onInput (ChangeNote note.id) ] []
+                ]
+
+        Nothing ->
+            text ""
